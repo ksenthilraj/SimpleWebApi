0b8e2a45bcb0f2c27a38f7cdcf5d81b62b2a6a59
ksenthilraj <36442391+ksenthilraj@users.noreply.github.com>
2025-11-21 05:30:58 +0530

    Refactor hello endpoint to use block syntax

--- SimpleWebApi/Program.cs
+++ SimpleWebApi/Program.cs
@@ -1,11 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Enable Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Enable Swagger UI in Development
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/api/hello", () => "Hello from GitHub Actions + Azure!");
+app.MapGet("/api/hello", () =>
+{
+    return "Hello from GitHub Actions + Azure!";
+});
 
 app.Run();
